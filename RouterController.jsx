de9fc3250ef9fac5f67429818d6b8a1239280a1c
stivcode21 <stivenelcodificador@gmail.com>
2025-03-15 22:47:21 -0500

    create a structure for the folders

---
+++ RouterController.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RouterController = () => {
+    return (
+        <div>RouterController</div>
+    )
+}
+
+export default RouterController
---
+++ src/pages/Guesbook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Guesbook = () => {
+    return (
+        <div>Guesbook</div>
+    )
+}
+
+export default Guesbook
