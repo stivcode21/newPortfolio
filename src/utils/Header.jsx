de0d8d068247e685490a293fb641eca3513a3c20
JeanCarlos <jmatochepascual@gmail.com>
2025-03-17 00:41:52 -0500

    feat: Refactor custom styles
    Improve the custon styles

--- src/utils/Header.jsx
+++ src/utils/Header.jsx
@@ -1,12 +1,16 @@
-import { MoonIcon } from "@heroicons/react/24/solid"
+import { MoonIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
-    return (
-        <div className='fixed top-0 left-0 right-0 h-[60px] flex items-center justify-between bg-opacity-50 z-100 bg-[var(--bg-dark)]/70 backdrop-filter backdrop-blur-lg padding-x'>
-            <img src="./logo-koalav01.png" alt="logo-koala-Stivcode" className="w-13 h-13" />
-            <MoonIcon className="w-8 h-8 text-white" />
-        </div>
-    )
-}
+  return (
+    <div className="fixed top-0 left-0 right-0 h-[60px] flex items-center justify-between bg-opacity-50 z-100 bg-dark/70 backdrop-filter backdrop-blur-lg px-2">
+      <img
+        src="./logo-koalav01.png"
+        alt="logo-koala-Stivcode"
+        className="w-13 h-13"
+      />
+      <MoonIcon className="w-8 h-8 text-white" />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
